fae0c1298a3c6e9ba1141a1322e00c15e47dd890
BoonPaulBenny <boon.benny@vvdntech.in>
2025-03-19 15:12:10 +0530

    updated producer & consumer functions

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -1,33 +1,37 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"log"
-
-	"github.com/segmentio/kafka-go"
 )
 
 func main() {
-	topic := "hello-topic"
-	partition := 0
+	topic := "Notification"
 
-	// Connect to Kafka
-	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
-	if err != nil {
-		log.Fatal("failed to connect to Kafka leader:", err)
-	}
-	defer conn.Close()
+	// Creating a kafka consumer
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "notification-group",
+		"auto.offset.reset": "earliest",
+	})
+	defer consumer.Close()
 
 	fmt.Println("Listening for messages on topic:", topic)
 
+	err = consumer.SubscribeTopics([]string{topic}, nil)
+	if err != nil {
+		fmt.Printf("Failed to subscribe to topic: %s\n", err)
+		return
+	}
+
 	for {
 		// Read message from Kafka
-		message, err := conn.ReadMessage(1e6) // 1MB max message size
-		if err != nil {
-			log.Fatal("failed to read message:", err)
+		message, consumerErr := consumer.ReadMessage(-1)
+		if consumerErr != nil {
+			log.Fatal("failed to read message:", consumerErr)
+		} else {
+			fmt.Printf("Received message: %s\n", string(message.Value))
 		}
-
-		fmt.Printf("Received message: %s\n", string(message.Value))
 	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module Kafka_POC
 go 1.23.6
 
 require (
-	github.com/klauspost/compress v1.15.9 // indirect
-	github.com/pierrec/lz4/v4 v4.1.15 // indirect
-	github.com/segmentio/kafka-go v0.4.47 // indirect
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/segmentio/kafka-go v0.4.47
 )
